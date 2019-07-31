3fec5e46ec66cd07dbfe22887d778c15bb7cda7a
nirmal-suresh <42872357+nirmal-suresh@users.noreply.github.com>
2019-08-01 00:24:05 +0530

    Add files via upload

---
+++ stringtoHex.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<string.h>
+int hexadecimalToDecimal(char hexVal[]) 
+{    
+    int len = strlen(hexVal); 
+      
+    // Initializing base value to 1, i.e 16^0 
+    int base = 1; 
+      
+    int dec_val = 0; 
+      
+    // Extracting characters as digits from last character 
+    for (int i=len-1; i>=0; i--) 
+    {    
+        // if character lies in '0'-'9', converting  
+        // it to integral 0-9 by subtracting 48 from 
+        // ASCII value. 
+        if (hexVal[i]>='0' && hexVal[i]<='9') 
+        { 
+            dec_val += (hexVal[i] - 48)*base; 
+                  
+            // incrementing base by power 
+            base = base * 16; 
+        } 
+  
+        // if character lies in 'A'-'F' , converting  
+        // it to integral 10 - 15 by subtracting 55  
+        // from ASCII value 
+        else if (hexVal[i]>='A' && hexVal[i]<='F') 
+        { 
+            dec_val += (hexVal[i] - 55)*base; 
+          
+            // incrementing base by power 
+            base = base*16; 
+        } 
+    } 
+      
+    return dec_val; 
+} 
+unsigned int stringToHex(char line[])
+{
+	unsigned int hex=0x0;
+	int n;
+	n=hexadecimalToDecimal(line);
+	hex=n;
+	return hex;
+}
+
+
+
+int main()
+{
+	char line[]="207A";
+	unsigned int hex;
+	hex=stringToHex(line);
+	printf("%x\n",hex );
+}
